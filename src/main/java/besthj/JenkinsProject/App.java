935176537769a414b6f263a3b9c2c50d7dbc8c6a
besthj <parkhjpp@daum.net>
2019-08-06 14:50:47 +0900

    - create Project

---
+++ src/main/java/besthj/JenkinsProject/App.java
@@ -0,0 +1,13 @@
+package besthj.JenkinsProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
