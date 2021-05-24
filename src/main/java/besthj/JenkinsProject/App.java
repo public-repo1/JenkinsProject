42ed4626e90941b441409c85889106bb39038de1
public-repo1 <81622240+public-repo1@users.noreply.github.com>
2021-05-24 16:00:02 +0900

    Update App.java

--- src/main/java/besthj/JenkinsProject/App.java
+++ src/main/java/besthj/JenkinsProject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World6625" );
+        System.out.println( "Hello World66255" );
     }
 }
